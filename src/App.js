f1a7770c3c5119ac9624be676ffb1a953c6c49c3
KshitizSachan <kshitizsachan422@gmail.com>
2023-04-07 15:03:56 +0530

    added hooks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
+    {/* Method 1 using hooks */}
+    <HooksCakeContainer />
+    
+    {/* Method 2 using connect function */}
      <CakeContainer/>
     </div>
     </Provider>
